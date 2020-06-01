feb161cc9f3ebca8d3e81a3e64de63f658a4f2d4
Gilson Silva <gilsonsilvati@gmail.com>
2020-06-01 19:53:28 -0300

    Exercício: implementando um CRUD

---
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/PrimeiroCrudTest.java
@@ -0,0 +1,60 @@
+package com.algaworks.ecommerce.iniciandocomjpa;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PrimeiroCrudTest extends EntityManagerTest {
+
+    @Test
+    public void buscar() {
+        var cliente = entityManager.find(Cliente.class, 2);
+
+        Assert.assertNotNull(cliente);
+    }
+
+    @Test
+    public void inserirRegistro() {
+        var cliente = new Cliente(3, "Leka Show Teste");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear(); // Necessário só para persist ou merge
+
+        var clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+
+        Assert.assertNotNull(clienteVerificacao);
+    }
+
+    @Test
+    public void atualizarRegistro() {
+        var cliente = entityManager.find(Cliente.class, 1);
+        cliente.setNome("Gabriel Show Teste");
+
+        entityManager.getTransaction().begin();
+        entityManager.getTransaction().commit();
+
+        entityManager.clear(); // Necessário só para persist ou merge
+
+        var clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+
+        Assert.assertEquals("Gabriel Show Teste", clienteVerificacao.getNome());
+    }
+
+    @Test
+    public void removerRegistro() {
+        var cliente = entityManager.find(Cliente.class, 2);
+
+        entityManager.getTransaction().begin();
+        entityManager.remove(cliente);
+        entityManager.getTransaction().commit();
+
+        var clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+
+        Assert.assertNull(clienteVerificacao);
+    }
+
+}
