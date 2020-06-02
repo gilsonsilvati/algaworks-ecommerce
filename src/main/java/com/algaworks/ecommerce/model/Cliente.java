c49b7e6c70dc47d08192d6e08e33dba01b57ee0c
Gilson Silva <gilsonsilvati@gmail.com>
2020-06-01 21:03:44 -0300

    Usando Lombok

--- src/main/java/com/algaworks/ecommerce/model/Cliente.java
+++ src/main/java/com/algaworks/ecommerce/model/Cliente.java
@@ -1,53 +1,26 @@
 package com.algaworks.ecommerce.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.Objects;
 
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "cliente")
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @NonNull
     @Id
     private Integer id;
 
+    @NonNull
     private String nome;
-
-    public Cliente() { }
-
-    public Cliente(Integer id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cliente cliente = (Cliente) o;
-        return id.equals(cliente.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 
 }
