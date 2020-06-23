4bb17af3e664569eac3362dd5c0013b793b3bbcf
Gilson Silva <gilsonsilvati@gmail.com>
2020-06-23 10:47:56 -0300

    Mapeando relacionamentos muitos-para-um

--- src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -2,7 +2,9 @@ package com.algaworks.ecommerce.relacionamentos;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.ItemPedido;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 import com.algaworks.ecommerce.model.enums.StatusPedido;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,14 +17,13 @@ import static org.junit.Assert.assertNotNull;
 public class RelacionamentoManyToOneTest extends EntityManagerTest {
 
     @Test
-    public void verificarRelacionamento() {
+    public void verificarRelacionamentoPedido() {
         var cliente = entityManager.find(Cliente.class, 1);
 
         var pedido = new Pedido();
         pedido.setStatus(StatusPedido.AGUARDANDO);
         pedido.setDataPedido(LocalDateTime.now());
         pedido.setTotal(BigDecimal.TEN);
-
         pedido.setCliente(cliente);
 
         entityManager.getTransaction().begin();
@@ -34,6 +35,36 @@ public class RelacionamentoManyToOneTest extends EntityManagerTest {
         var pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
 
         assertNotNull(pedidoVerificacao.getCliente());
+    }
+
+    @Test
+    public void verificarRelacionamentoItemPedido() {
+        var cliente = entityManager.find(Cliente.class, 1);
+        var produto = entityManager.find(Produto.class, 1);
+
+        var pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+        pedido.setCliente(cliente);
+
+        var itemPedido = new ItemPedido();
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1);
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.persist(itemPedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        var itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+
+        assertNotNull(itemPedidoVerificacao.getPedido());
+        assertNotNull(itemPedidoVerificacao.getProduto());
     }
 
 }
