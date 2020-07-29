1436193efac6682599859ab9e2e6e82b0683b446
Gilson Silva <gilsonsilvati@gmail.com>
2020-07-28 21:52:41 -0300

    Removendo objetos referenciados por outras entidades

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,5 +1,9 @@
-insert into produto (nome, preco, descricao) values ('Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
-insert into produto (nome, preco, descricao) values ('Câmera GoPro Hero 7', 1400.0, 'Desempenho 2x melhor.');
+insert into produto(nome, preco, descricao) values('Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
+insert into produto(nome, preco, descricao) values('Câmera GoPro Hero 7', 1400.0, 'Desempenho 2x melhor.');
 
-insert into cliente (nome) values ('Gilson Show Teste');
-insert into cliente (nome) values ('Lidy Show Teste');
+insert into cliente(nome) values('Gilson Show Teste');
+insert into cliente(nome) values('Lidy Show Teste');
+
+insert into pedido(id, cliente_id, data_pedido, total, status) values(1, 1, sysdate(), 100.0, 'AGUARDANDO');
+
+insert into item_pedido(id, pedido_id, produto_id, preco_produto, quantidade) values(1, 1, 1, 5.0, 2);
---
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/RemovendoEntidadesReferenciadasTest.java
@@ -0,0 +1,29 @@
+package com.algaworks.ecommerce.relacionamentos;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
+public class RemovendoEntidadesReferenciadasTest extends EntityManagerTest {
+
+    @Test
+    public void deve_remover_entidade_relacionada() {
+        var pedido = entityManager.find(Pedido.class, 1);
+
+        Assert.assertFalse(pedido.getItens().isEmpty());
+
+        entityManager.getTransaction().begin();
+        pedido.getItens().forEach(item -> entityManager.remove(item));
+        entityManager.remove(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        var pedidoVerificacao = entityManager.find(Pedido.class, 1);
+        assertNull(pedidoVerificacao);
+    }
+
+}
