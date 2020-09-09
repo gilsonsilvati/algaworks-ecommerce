8b1fef09972066055540e17a348e79b87c5a4bee
Gilson Silva <gilsonsilvati@gmail.com>
2020-09-09 14:29:54 -0300

    Limitando a quantidade de registros retornados

--- src/test/java/com/algaworks/ecommerce/jpql/PaginacaoJPQLTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/PaginacaoJPQLTest.java
@@ -11,6 +11,19 @@ import java.util.List;
 public class PaginacaoJPQLTest extends EntityManagerConfig {
 
     @Test
+    public void limitarResultados() {
+        String jpql = "select c from Categoria c order by c.nome";
+
+        TypedQuery<Categoria> typedQuery = entityManager.createQuery(jpql, Categoria.class);
+        typedQuery.setMaxResults(4);
+
+        List<Categoria> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(c -> System.out.println(c.getId() + ", " + c.getNome()));
+    }
+
+    @Test
     public void paginarResultados() {
         String jpql = "select c from Categoria c order by c.nome";
 
@@ -18,7 +31,7 @@ public class PaginacaoJPQLTest extends EntityManagerConfig {
 
         // FIRST_RESULT = MAX_RESULTS * (pagina - 1)
         typedQuery.setFirstResult(6);
-        typedQuery.setMaxResults(2);
+        typedQuery.setMaxResults(2); // Normalmente é um valor fixo
 
         List<Categoria> lista = typedQuery.getResultList();
         Assert.assertFalse(lista.isEmpty());
