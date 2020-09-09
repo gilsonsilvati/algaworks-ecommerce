142baf6f456352d14e4912651ab0e3dd3879d3e2
Gilson Silva <gilsonsilvati@gmail.com>
2020-09-09 12:04:51 -0300

    Usando expressão de diferente

--- src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -14,6 +14,16 @@ import java.util.List;
 public class ExpressoesCondicionaisTest extends EntityManagerConfig {
 
     @Test
+    public void usarExpressaoDiferente() {
+        String jpql = "select p from Produto p where p.preco <> 100";
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
     public void usarBetween() {
         String jpql = "select p from Pedido p " +
                 " where p.dataCriacao between :dataInicial and :dataFinal";
